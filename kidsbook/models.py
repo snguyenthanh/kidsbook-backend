c571c387271dcd3157397112994608bc437d900a
Le Trung Hieu <lth08091998@gmail.com>
2018-10-12 22:27:31 +0800

    Add user

--- kidsbook/models.py
+++ kidsbook/models.py
@@ -1,8 +1,67 @@
+from __future__ import unicode_literals
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+import uuid
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, email_address, password, **extra_fields):
+        """
+        Creates and saves a User with the given username, email and password.
+        """
+        if not username:
+            raise ValueError('The given username must be set')
+        email_address = self.normalize_email(email_address)
+        user = self.model(username=username, email_address=email_address, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email_address=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email_address, password, **extra_fields)
+
+    def create_superuser(self, username, email_address, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, email_address, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email_address = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=128)
+    is_active = models.BooleanField(default=True)
+
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+
+    USERNAME_FIELD = 'email_address'
+    EMAIL_FIELD = 'email_address'
+
+    REQUIRED_FIELDS = ["username"]
+    objects = UserManager()
 
 class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
