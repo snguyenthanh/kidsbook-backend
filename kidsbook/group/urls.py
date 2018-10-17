98e2711e49d3acbee4328b14dd4549e978980ad2
Son <son@fr0st.local>
2018-10-17 12:32:14 +0800

    Added API for Delete group - Clean up and re-structure other Group Apis

--- kidsbook/group/urls.py
+++ kidsbook/group/urls.py
@@ -3,5 +3,12 @@ from kidsbook.group import views
 
 urlpatterns = [
     #path('api-auth/', include('rest_auth.urls')),
-    path('', views.get_groups)
+
+    # Return all groups or create a new group
+    path('', views.group),
+
+    # Add new member or remove a member in a group
+    path('<group_id>/user/<user_id>', views.group_member),
+
+    path('<group_id>', views.delele_group)
 ]
--- kidsbook/models.py
+++ kidsbook/models.py
@@ -116,7 +116,7 @@ class GroupManager(models.Manager):
 
 class Group(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=120)
+    name = models.CharField(max_length=120, unique=True)
     description = models.TextField(blank=True)
     creator = models.ForeignKey(User, related_name='group_owner', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
