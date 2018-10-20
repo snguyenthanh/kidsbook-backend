cb746ef0955fc778dcfadb95b665df90ab393ec9
Sreyans Sipani <sreyans.sipani99@gmail.com>
2018-10-21 03:13:48 +0800

    Revert settings for deployment

--- backend/settings.py
+++ backend/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'app_db',
-#         'USER': 'app_user',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app_db',
+        'USER': 'app_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
--- run_fixtures.sh
+++ run_fixtures.sh
@@ -2,7 +2,7 @@ FIXTURES="fixtures.py"
 rm db.sqlite3
 rm -r kidsbook/migrations
 echo "Deleting existing database."
-#psql -d app_db -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public; GRANT ALL ON SCHEMA public TO public;"
+psql -d app_db -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public; GRANT ALL ON SCHEMA public TO public;"
 echo "Generating new migrations."
 python3 manage.py makemigrations kidsbook
 echo "Migrating database."
