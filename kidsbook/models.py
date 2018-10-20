988a6a755c437acf34883cdbdb0bc5357a7334cf
Sreyans Sipani <sreyans.sipani99@gmail.com>
2018-10-20 16:27:55 +0800

    Uncomment post manager

--- kidsbook/models.py
+++ kidsbook/models.py
@@ -207,7 +207,7 @@ class Post(models.Model):
 
     REQUIRED_FIELDS = ["content"]
 
-    # objects = PostManager()
+    objects = PostManager()
     class Meta:
         ordering = ('created_at',)
 
