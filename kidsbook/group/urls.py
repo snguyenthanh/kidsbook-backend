b4e34acc097b6ab1b85164ed31f947412a1495d7
Le Trung Hieu <lth08091998@gmail.com>
2018-10-20 15:46:43 +0800

    Refactor group api

--- kidsbook/group/urls.py
+++ kidsbook/group/urls.py
@@ -6,7 +6,12 @@ urlpatterns = [
     path('', views.group),
 
     # Add new member or remove a member in a group
-    path('<group_id>/user/<user_id>/', views.group_member),
+    path('<uuid:group_id>/user/<uuid:user_id>/', views.group_member),
 
-    path('<group_id>/', views.delete_group)
+    # View all members public profile of this group
+    path('<uuid:group_id>/user/', views.get_all_user),
+
+    path('<uuid:group_id>/', views.delete_group),
+
+    # path('<group_id')
 ]
