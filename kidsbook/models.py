b9b530ebf621e8581c96c70217bb9a33868900d0
Sreyans Sipani <sreyans.sipani99@gmail.com>
2018-10-24 02:11:46 +0800

    Flag fix

--- kidsbook/models.py
+++ kidsbook/models.py
@@ -269,4 +269,4 @@ class UserFlagPost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True)
-    status = models.CharField(max_length=120, unique=True)
+    status = models.CharField(max_length=120)
