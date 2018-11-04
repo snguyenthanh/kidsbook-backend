ad7ba7b96e7d7c8eab0d66f9d20ac5b23d0862d1
Son <son@fr0st.local>
2018-11-04 10:33:36 +0800

    Fix params error for push_notification and add error handler

--- kidsbook/utils.py
+++ kidsbook/utils.py
@@ -12,9 +12,16 @@ def clean_data_iterative(data, *args):
             row.pop(field, None)
     return data
 
-def push_notification(**kargs):
+def push_notification(send_data: dict):
     kargs['secretKey'] = getattr(settings, "NOTIFICATION_SECRET_KEY", None)
     url = getattr(settings, "NOTIFICATION_ENDPOINT", None)
 
-    response = requests.post(url, data=kargs)
-    
+    try:
+        response = requests.post(
+                        url,
+                        data=send_data,
+                        headers={'Connection': 'close'},
+                        timeout=2       # 2 seconds
+                    )
+    except Exception:
+        pass
